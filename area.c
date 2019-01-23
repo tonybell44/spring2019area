d38cf6edbae2fbce833b7836746e6491dc24bd89
Tony Belladonna <belladon@cooper.edu>
2019-01-22 20:45:04 -0500

    committed area

---
+++ area.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define PI 3.14
+
+int main() {
+	
+	int radius = 2;
+	float area = PI * radius * radius;
+	printf("area of circle with radius %d = %f\n",radius,  area);
+	return 0;
+}
